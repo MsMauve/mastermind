1f1a18112244675ba539fe2e3aa634b5f646d675
MsMauve <zoetropesilver@gmail.com>
2025-06-12 17:05:05 -0600

    add role choice and basic computer guess

--- game.rb
+++ game.rb
@@ -1,8 +1,6 @@
 class Game
   def initialize
-    @board = Board.new_amount
-    @player = Player.new("Player 1")
-    @board.create_secret_code
+    setup_game
   end
 
   def play
@@ -23,5 +21,43 @@ class Game
     when :lose
       puts "I mean, it's called Enigma for a reason..."
     end
+  end
+
+  private
+
+  def setup_game
+    @board = Board.new
+    choose_role
+    setup_players
+  end
+
+  def choose_role
+    puts "Would you like to make the code, or try to guess it?"
+    puts "1. Guesser"
+    puts "2. Maker"
+
+    loop do
+      puts "Enter your choice (1 or 2): "
+      choice = gets.chomp.to_i
+      if [1, 2].include?(choice)
+        return choice == 1? :guesser : :maker
+      end
+      puts "Invalid choice: 1 or 2?"
+    end
+  end
+
+  def setup_players
+    role = choose_role
+    @player = Player.new("Player 1", role)
+    if role == :maker
+      code = @player.create_code(@board.class::COLORS)
+      @board.set_code(code)
+    else
+      @board.create_secret_code
+    end
+  end
+
+  def computer_guess
+    Array.new(4) { @board.class::COLORS.sample }
   end
 end
