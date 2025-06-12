7cc658efc201cbd76425da56857f5c79fabaa589
MsMauve <zoetropesilver@gmail.com>
2025-06-12 16:49:06 -0600

    add Player class and logic

---
+++ player.rb
@@ -0,0 +1,30 @@
+class Player
+  attr_reader :name, :role
+
+  def initialize(name, role = :guesser)
+    @name = name
+    @role = role
+  end
+
+  def make_guess(available_colors)
+    puts "#{@name}'s turn"
+    puts "Available colors: #{available_colors.join(', ')}"
+    loop do
+      print "Enter four colors (separated with a space): "
+      input = gets.chomp.downcase.split
+
+      if valid_guess?(input, available_colors)
+        return input.map(&:to_sym)
+      else
+        puts "Sorry, you gotta enter 4 colors that are valid!"
+      end
+    end
+  end
+
+  private
+
+  def valid_guess?(input, available_colors)
+    return false unless input.length == 4
+    input.all? { |color| available_colors.include?(color.to_sym) }
+  end
+end
